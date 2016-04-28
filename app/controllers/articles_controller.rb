176a993d328443bfe5d294d2bf2faea6c8497c73
DANIEL CUPERMAN <dacuperman@gmail.com>
2016-04-28 16:10:58 -0700

    form based workflow commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,48 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
+  class ArticlesController < ApplicationController
+    include ArticlesHelper
+    def index
+      @articles = Article.all
+    end
 
-  def show
-    @articles = Article.find(params[:id])
+    def show
+      @articles = Article.find(params[:id])
+    end
+
+    def new
+      @articles = Article.new
+    end
+
+    def create
+      @articles = Article.new(article_params)
+      @articles.title = params[:article][:title]
+      @articles.save
+
+      flash.notice = "Article '#{@articles.title}' Created!"
+
+      redirect_to article_path(@articles)
+    end
+
+    def destroy
+      @articles = Article.find(params[:id])
+      @articles.destroy
+
+      flash.notice = "Article '#{@articles.title}' Deleted!"
+
+      redirect_to articles_path
+    end
+
+    def edit
+      @articles = Article.find(params[:id])
+    end
+
+    def update
+      @articles = Article.find(params[:id])
+      @articles.update (article_params)
+
+      flash.notice = "Article '#{@articles.title}' Updated!"
+
+      redirect_to article_path(@articles)
+
+    end
+
   end
-end
